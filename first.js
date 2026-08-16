576f2ac2c8fa1fe52d28ff9e99f0f51c2baf961f
Noel Rego <20leonarego@gmail.com>
2026-08-16 15:41:34 +0530

    adding logic to js

--- first.js
+++ first.js
@@ -1,7 +1,15 @@
-const invisibleButtons=document.querySelector(".invisibleButtons");
+const invisibleButtons = document.querySelector(".invisibleButtons");
+const button = document.querySelector("button");
 
+let clickCount = 0;
 
-invisibleButtons.addEventListener("click",()=>{
+button.addEventListener("click", () => {
+    clickCount++; 
 
-}
-)
+    if (clickCount === 1) {
+        invisibleButtons.style.display = "flex";
+    } else if (clickCount === 2) {
+        invisibleButtons.style.display = "none";
+        clickCount = 0; 
+    }
+});
