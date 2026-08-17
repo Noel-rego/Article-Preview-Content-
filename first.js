c58d337bfb75b5f8e33b0a7cd04efec4e05baf7f
Noel Rego <20leonarego@gmail.com>
2026-08-18 00:20:38 +0530

    debugging

--- first.js
+++ first.js
@@ -1,15 +1,24 @@
 const invisibleButtons = document.querySelector(".invisibleButtons");
-const button = document.querySelector("button");
+const button = document.querySelector(".share-btn");
+const endBody=document.querySelector(".endBody");
 
 let clickCount = 0;
 
-button.addEventListener("click", () => {
-    clickCount++; 
-
-    if (clickCount === 1) {
-        invisibleButtons.style.display = "flex";
-    } else if (clickCount === 2) {
-        invisibleButtons.style.display = "none";
-        clickCount = 0; 
+button.addEventListener("click",() => {
+    let screenWidth = window.innerWidth; 
+    if (screenWidth<=768){
+        endBody.classList.toggle("active");
+        console.log("device is in moblile view,button was clicked.") ;
     }
-});
+    else{
+        clickCount++;
+        if(clickCount===1){
+            invisibleButtons.style.display = "flex";
+        }
+        else if(clickCount===2){
+            invisibleButtons.style.display="none";
+            clickCount=0;
+        }
+    }
+ });
+
